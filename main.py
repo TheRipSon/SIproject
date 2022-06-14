f8207c166ce0391b9f7f22f4a4e1962d7f6e7fa8
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-06-14 23:08:39 +0200

    asd

--- main.py
+++ main.py
@@ -8,20 +8,26 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
-newlabels= {}
+newlabels = {}
 
 
-def max_flow(c, s, t):
-    n = len(c)  # C is the capacity matrix
+def max_flow(c, s, t, n):
     f = [[0] * n for i in range(n)]
     path = bfs(c, f, s, t)
     #  print path
+
     while path != None:
         flow = min(c[u][v] - f[u][v] for u, v in path)
         for u, v in path:
             f[u][v] += flow
             f[v][u] -= flow
+            a = (u, v)
+            z=list(newlabels[a].keys())[0]
+            newlabels[a][z]+=flow
+            print(f"{u},{v} : {newlabels[a]}")
+        print("XD")
         path = bfs(c, f, s, t)
+
     return sum(f[s][i] for i in range(n))
 
 
@@ -29,6 +35,7 @@ def max_flow(c, s, t):
 def bfs(c, f, s, t):
     queue = [s]
     paths = {s: []}
+    u = 0
     if s == t:
         return paths[s]
     while queue:
@@ -39,7 +46,7 @@ def bfs(c, f, s, t):
                 if v == t:
                     return paths[v]
                 queue.append(v)
-    print(paths)
+    print(paths[u])
     return None
 
 
@@ -47,12 +54,14 @@ def bfs(c, f, s, t):
 if __name__ == '__main__':
     # make a capacity graph
     # node   s   o   p   q   r   t
-    C = [[0, 3, 3, 0, 0, 0],  # s
-         [0, 0, 2, 3, 0, 0],  # o
-         [0, 0, 0, 0, 2, 0],  # p
-         [0, 0, 0, 0, 4, 2],  # q
-         [0, 0, 0, 0, 0, 2],  # r
-         [0, 0, 0, 0, 0, 3]]  # t
+    C = [[0, 3, 3, 0, 0, 0,,],  # s
+         [0, 0, 2, 3, 0, 0,,],  # o
+         [0, 0, 0, 0, 2, 0,,],  # p
+         [0, 0, 0, 0, 4, 2,,],  # q
+         [0, 0, 0, 0, 0, 2,,],  # r
+         [0, 0, 0, 0, 0, 2,,],
+         [0, 0, 0, 0, 0, 2,,],
+         [0, 0, 0, 0, 0, 3,,]]  # t
 
     start = 0  # A
     end = 5  # F
@@ -61,15 +70,11 @@ if __name__ == '__main__':
     G = nx.DiGraph(A)
     labels = nx.get_edge_attributes(G, "weight")
     for i in labels:
-        newlabels[i]={0:labels[i]}
-    max_flow_value = max_flow(C, start, end)
+        newlabels[i] = {labels[i]:0}
+    max_flow_value = max_flow(C, start, end, n)
     print("Edmonds-Karp algorithm")
     print("max_flow_value is: ", max_flow_value)
-
     pos = nx.spring_layout(G)
-
-
-
     nx.draw(G, pos, with_labels=True)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=newlabels)
     plt.show()
