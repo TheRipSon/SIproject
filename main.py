17a09668b27e4bccea868f03b12f7058f33638fe
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-06-18 21:26:20 +0200

    asd

--- main.py
+++ main.py
@@ -96,19 +96,13 @@ def build_graph(matrix, labels):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     root = tk.Tk()
-    # make a capacity graph
-    #     s  1  2  3  4  5  6  t
-    C = [[0, 8, 0, 5, 0, 1, 0, 0],  # s
-         [0, 0, 7, 0, 0, 0, 0, 0],  # 1
-         [0, 0, 0, 0, 6, 0, 0, 2],  # 2
-         [0, 5, 0, 0, 3, 5, 0, 0],  # 3
-         [0, 0, 0, 0, 0, 0, 0, 6],  # 4
-         [0, 0, 0, 0, 0, 0, 6, 0],  # 5
-         [0, 0, 0, 0, 0, 0, 0, 7],  # 6
-         [0, 0, 0, 0, 0, 0, 0, 0]]  # t
 
-    start = 0  # A
-    end = 7  # F
+    with open('input.txt', 'r') as q:
+        start, end = q.readline().split(",")
+        C = [[int(num) for num in line.split(',')] for line in q]
+    start = int(start)
+    end = int(end)
+
     A = np.matrix(C)
     n = len(C)
     G = nx.DiGraph(A)
